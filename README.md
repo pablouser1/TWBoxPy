31a458974ee23bfa3e8d66104dec6b99030bf5a2
Pablo Ferreiro <me@pabloferreiro.es>
2023-07-28 17:57:56 +0200

    Region

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Simple box art downloader from GameTDB deisgned for TwilightMenu++
 
 ## Features
 * Cross-platform (Windows, Mac and Linux)
-* Multi-language
+* Multi-region
 * Ready to use for TwilightMenu++
 
 ## Requirements
@@ -18,10 +18,30 @@ pip install -r requirements.txt
 ## Usage
 ```bash
 # In this example, the program will scan nds files from IN_DIR,
-# try to download covers in english, spanish and then japanese
+# try to download covers for the United Kingdom, then Spain and then Japan
 # and save all covers in ./out
-python3 twbox.py IN_DIR --languages EN ES JA --output ./out
+python3 twbox.py IN_DIR --regions EN ES JA --output ./out
 ```
+
+### Regions
+| Name | Code |
+| :--: | :--: |
+| United States | US |
+| Japan | JA |
+| United Kingdom | EN |
+| France | FR |
+| Germany | DE |
+| Spain | ES |
+| Italy | IT |
+| Netherlands | NL |
+| Portugal | PT |
+| Australia | AU |
+| Sweden | SE |
+| Denmark | DK |
+| Norway | NO |
+| Finland | FI |
+| Korea | KO |
+| Russia | RU |
 
 You can get more info about how to use the CLI tool with:
 ```bash
--- twbox.py
+++ twbox.py
@@ -3,7 +3,7 @@ import argparse
 from glob import glob
 import requests
 
-BASE_URL = "https://art.gametdb.com/ds/coverS/{language}/{ndsId}.png"
+BASE_URL = "https://art.gametdb.com/ds/coverS/{region}/{ndsId}.png"
 
 def printVerbose(msg: str, enabled: bool):
     if enabled:
@@ -20,15 +20,15 @@ def getRomInfo(path: str)-> tuple:
         ndsIdCode = f.read(4)
     return ndsTitle.decode("utf8"), ndsIdCode.decode("utf8")
 
-def downloadArt(ndsId: str, langs: list, output: str, verbose: bool)-> tuple:
+def downloadArt(ndsId: str, regions: list, output: str, verbose: bool)-> tuple:
     success = False
     code = -1
 
     i = 0
-    while i < len(langs) and not success:
-        lang = langs[i]
-        printVerbose("Trying to download {0}, {1}".format(ndsId, lang), verbose)
-        url = BASE_URL.format(language=lang, ndsId=ndsId)
+    while i < len(regions) and not success:
+        region = regions[i]
+        printVerbose("Trying to download {0}, {1}".format(ndsId, region), verbose)
+        url = BASE_URL.format(region=region, ndsId=ndsId)
         filename = os.path.basename(url)
         r = requests.get(url, stream=True)
         if r.ok:
@@ -49,7 +49,7 @@ def main():
     
     parser.add_argument('input')
     parser.add_argument('-o', '--output', default="./out", help="Output folder where the images will be downloaded, defaults to ./out")
-    parser.add_argument('-l', '--languages', nargs="+", default=['EN'], help="Languages to download the covers from top to bottom priority. Defaults to EN. Available: EN, ES, FR, IT...")
+    parser.add_argument('-r', '--regions', nargs="+", default=['EN'], help="Regions to download the covers from top to bottom priority. Defaults to EN. Available: EN, ES, FR, IT...")
     parser.add_argument('-v', '--verbose', action='store_true', default=False, help="Make program verbose")
 
     args = parser.parse_args()
@@ -68,7 +68,7 @@ def main():
         ndsName, ndsId = getRomInfo(f)
         if not os.path.exists(os.path.join(args.output, "{0}.png".format(ndsId))):
             print("Getting {0}: {1}".format(ndsName, ndsId))
-            success, code = downloadArt(ndsId, args.languages, args.output, args.verbose)
+            success, code = downloadArt(ndsId, args.regions, args.output, args.verbose)
 
             if not success:
                 print("Could not download cover art for {0}! HTTP Code {1}".format(ndsName, code))
